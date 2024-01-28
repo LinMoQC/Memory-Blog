ff2ec40595c6d3e807e4b8fbc50b2647e801ea03
Karensky <2896311434@qq.com>
2024-01-28 18:34:25 +0800

    每日箴言

--- src/pages/Dashboard/Home/index.tsx
+++ src/pages/Dashboard/Home/index.tsx
@@ -1,7 +1,36 @@
-const Home = () => {
-    return <div>
-        <h1>Home</h1>
-    </div>
-}
+import { Card } from "antd";
+import './index.sass';
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default Home
+const Home = () => {
+    const [oneSay, setOneSay] = useState('');
+
+    useEffect(() => {
+        const getSay = async () => {
+            const res = await axios.get('https://zj.v.api.aa1.cn/api/wenan-zl/?type=json');
+            setOneSay(res.data.msg);
+        };
+        getSay();
+    }, []);
+
+    return (
+        <div className="home">
+            <Card size="small" title={
+                <div className="custom-card-header">
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                    <span className="dot"></span>
+                    每日箴言
+                </div>
+            } style={{ maxWidth: 350, height: 200 }}>
+                <div className="oneSay">
+                    <span className="stick">📌</span>
+                    <p className="onesay_content">{oneSay}</p>
+                </div>
+            </Card>
+        </div>
+    );
+};
+
+export default Home;
