6263c85691ba1469d41813949e0ed608be347699
Karensky <2896311434@qq.com>
2024-04-30 21:28:39 +0800

    全部文章新增图片预览

--- src/apis/axios.tsx
+++ src/apis/axios.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import getToken from "./getToken.tsx"
 
-const baseURL = 'http://127.0.0.1:8080';
+const baseURL = 'http://127.0.0.1';
 
 const http = axios.create({
     baseURL: baseURL,
